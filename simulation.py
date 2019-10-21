ac29c1b3292647b9d83b3ac75f3b0b091e4e0d79
Krisztian Markella <chris.markella@gmail.com>
2019-10-21 13:18:39 -0700

    match listed game by game

--- simulation.py
+++ simulation.py
@@ -1,3 +1,5 @@
+# YouTube: https://www.youtube.com/watch?v=aMy3dYt5itE&t=748s 12:31
+
 import axelrod as axl
 
 player1 = axl.Cooperator()
@@ -5,5 +7,5 @@ player2 = axl.Defector()
 player3 = axl.TitForTat()
 
 match = axl.Match([player1, player2], 5)
-match.play()
+print(f'playing the match: {match.play()}')
 print(f'final score: {match.final_score()}')
