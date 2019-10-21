80ea5aa6e3d3ce762392c149c47db98d07b356b5
Krisztian Markella <chris.markella@gmail.com>
2019-10-21 13:20:34 -0700

    Another match added between Defecotor and TitForTat with 10 rounds.

--- simulation.py
+++ simulation.py
@@ -9,3 +9,7 @@ player3 = axl.TitForTat()
 match = axl.Match([player1, player2], 5)
 print(f'playing the match: {match.play()}')
 print(f'final score: {match.final_score()}')
+
+match2 = axl.Match([player2, player3], 10)
+print(f'playing the match: {match2.play()}')
+print(f'final score: {match2.final_score()}')
