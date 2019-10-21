0a3388bccd2d3a151d102431449095fcf0885b84
Krisztian Markella <chris.markella@gmail.com>
2019-10-21 13:15:49 -0700

    a match added between two players. Defector beat Cooperator.

--- simulation.py
+++ simulation.py
@@ -3,3 +3,7 @@ import axelrod as axl
 player1 = axl.Cooperator()
 player2 = axl.Defector()
 player3 = axl.TitForTat()
+
+match = axl.Match([player1, player2], 5)
+match.play()
+print(f'final score: {match.final_score()}')
